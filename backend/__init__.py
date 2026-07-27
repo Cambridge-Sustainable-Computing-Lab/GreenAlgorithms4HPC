8899f626ccaf1bf2b24eb53da89fcfe7445bcc02
Navirah Kamal <navirahkamal@gmail.com>
2026-07-27 11:00:08 +0100

    pandas loc 0 fix

--- backend/__init__.py
+++ backend/__init__.py
@@ -76,7 +76,7 @@ def summarise_data(df, args):
     df_userdaily = agg_jobs(df, ['SubmitDate'])
     df_overallStats = agg_jobs(df_userdaily)
     dict_overallStats = df_overallStats.iloc[0, :].to_dict()
-    userID = df.UserX[0]
+    userID = df.UserX.iloc[0]
 
     output = {
         "userDaily": df_userdaily,
